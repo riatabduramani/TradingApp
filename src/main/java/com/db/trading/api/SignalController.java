6d1341236ec954147ccf0595abc0e042204ebd23
Riat Abduramani <riat.abduramani@justeattakeaway.com>
2022-12-01 09:26:56 +0100

    Remove SignalProcess from the import in the Controller

--- src/main/java/com/db/trading/api/SignalController.java
+++ src/main/java/com/db/trading/api/SignalController.java
@@ -1,7 +1,6 @@
 package com.db.trading.api;
 
 import com.db.library.algolib.SignalHandler;
-import com.db.trading.signal.SignalProcessor;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
